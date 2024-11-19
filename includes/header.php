ab4ab57760633aa3b64f245766b34ef531a129ce
JeloDev <angelojosephjeremias8501@gmail.com>
2024-11-19 20:33:47 +0800

    change some code in heade, index, and styles

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,6 @@
 
   <head>
     <meta charset="utf-8">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CRoboto" rel="stylesheet">
     <meta http-equiv="x-ua-compatible" content="IE=edge, chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   
@@ -12,7 +11,7 @@
     <title>Alfa SportsCity </title>
     
     <link href="styles/bootstrap.min.css" rel="stylesheet">
-    <link href="styles/backend.css" rel="stylesheet">
+    <!-- <link href="styles/backend.css" rel="stylesheet"> -->
     <link href="styles/style.css" rel="stylesheet">
   
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     
     <title>Alfa SportsCity </title>
     <link href="styles/style.css" rel="stylesheet">
-    <link href="styles/backend.css" rel="stylesheet">
+    <!-- <link href="styles/backend.css" rel="stylesheet"> -->
     <link href="styles/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -109,38 +109,45 @@
   </head>
   <body>
       <main>
-        <div class="container row">
-          <div class="img-logo col-4">
-            <img src="images/alfa-removebg-preview.png" alt="alfa logo" width="100" class=" rounded-circle" >
+        <div class="container">
+          <div class="row">
+            <div class="img-logo">
+              <img src="images/alfa-removebg-preview.png" alt="alfa logo" width="100" class=" rounded-circle" >
+            </div>
+          
+            <div class="container">
+              <p class="">Welcome to Alfa SportsCity</pc>
+            </div>
+
+            <form action="" method="POST" class="p-3">
+
+              <div class="form-group form-floating m-2">
+
+                <input type="email" class="form-control " id="email" name="c_email" placeholder="juandelacruz@gmail.com" required>
+
+                <label for="email" class="form-label">Email address</label>
+
+                <div class="valid-feedback">Valid.</div>
+
+                <div class="invalid-feedback">Please fill out this field.</div>
+
+              </div>
+              <div class="form-group form-floating m-2">
+
+                <input type="password" class="form-control" id="pwd" name="c_pass" placeholder="">
+
+                <label for="email" class="form-label">Password</label>
+
+                <div class="valid-feedback">Valid.</div>
+                
+                <div class="invalid-feedback">Please fill out this field.</div>
+              </div>
+              <div class="form-group">
+                <button name="login" value="Login" class="btn btn-primary p-2 mt-2 btn-lg" >Login</button>
+              </div>
+
+            </form>
           </div>
-        
-          <div class="container col-sm-2 text-dark">
-            <p>Welcome to Alfa SportsCity</p>
-          </div>
-
-          <form action="" method="POST" class="p-3">
-
-           <div class="form-group form-floating m-2">
-              <input type="email" class="form-control" id="email" name="c_email" placeholder="juandelacruz@gmail.com" required>
-              <label for="email" class="form-label">Email</label>
-              <div class="valid-feedback">Valid.</div>
-              <div class="invalid-feedback">Please fill out this field.</div>
-            </div>
-            <div class="form-group form-floating m-2">
-              <input type="password" class="form-control" id="pwd" name="c_pass" >
-              <label for="password" class="form-label">Password</label>
-              <div class="valid-feedback">Valid.</div>
-              <div class="invalid-feedback">Please fill out this field.</div>
-            </div>
-            <div class="form-group">
-              <button name="login" value="Login" class="btn btn-primary p-2 mt-2 btn-lg" >
-
-                <i class="fa fa-sign-in" ></i> Login
-
-              </button>
-            </div>
-
-          </form>
         </div>
       </main>
 
